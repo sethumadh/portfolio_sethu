2b52f753ae8dde0f0d551c3dc082ebeb9c4d7f59
sethu <sethumadh@gmail.com>
2023-01-24 16:35:58 +1000

    eslint disbale

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": "plugin:react/recommended",
+    "extends": "next",
     "overrides": [
     ],
     "parser": "@typescript-eslint/parser",
@@ -16,5 +16,8 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
-    }
+        "react/no-unescaped-entities": "off",
+        "@next/next/no-page-custom-font": "off"
+    },
+    
 }
